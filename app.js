a6b6e31ddb8e0f9bb9e081eae03c0bf8d1ee9787
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2024-09-24 00:13:44 +0600

    donate function added

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
+ 
+ const accountBalance = parseFloat(document.getElementById('account-balance').innerText);
 
 
+ const feniAmount =parseFloat (document.getElementById('input-amount-feni').value);
+ const feniBalance = parseFloat(document.getElementById('feni-balance').innerText);
+
+ const quotaAmount =parseFloat (document.getElementById('input-amount-quota').value);
+ const quotaBalance = parseFloat(document.getElementById('quota-balance').innerText);
 
 
-function donateMoney(event) {
-    const accountBalance = parseFloat(document.getElementById('account-balance').innerText);
-    const noakhaliAmount =parseFloat (document.getElementById('input-amount-noakhali').value);
-    const noakhaliBalance = parseFloat(document.getElementById('noakhali-balance').innerText);
+ document.getElementById('donate-Noakhali').addEventListener('click',function(){
+   const accountBalance = parseFloat(document.getElementById('account-balance').innerText);
+   const noakhaliAmount =parseFloat (document.getElementById('input-amount-noakhali').value);
+   const noakhaliAmountEl =document.getElementById('input-amount-noakhali');
+   const noakhaliBalance = parseFloat(document.getElementById('noakhali-balance').innerText);
     
-    const newAmount =  noakhaliAmount +noakhaliBalance;
+   noakhaliAmountEl.value = '';
+   
+ const {newBalance, remainingBalance }=  donateMoney(accountBalance,noakhaliAmount,noakhaliBalance)
+   
+  if(newBalance !== undefined){
+   document.getElementById('noakhali-balance').innerText =newBalance;
+   document.getElementById('account-balance').innerText = remainingBalance;
+   modal();
+  }
 
-    console.log(newAmount)
-     
-    document.getElementById('noakhali-balance').innerText =newAmount;
+ });
 
-   modal()
+
+
+
+function donateMoney(accountBalance, amount,balance) {
+       
+   if(amount > 0 && accountBalance >= amount){
+    const newBalance = balance + amount;
+   const remainingBalance = accountBalance - amount;
+     return {newBalance, remainingBalance }; 
+   }else{
+       alert('invalid input');
+       return undefined;
+   }  
 
 }
-
 
 
 
@@ -26,11 +51,11 @@ function donateMoney(event) {
     document.getElementById('donation-section').classList.add('hidden');
     document.getElementById('history-section').classList.remove('hidden');
 
-   historyBtn.classList.add('bg-btn-color');
+   historyBtn.classList.add('bg-btn-color','hover:bg-btn-color');
    historyBtn.classList.remove('bg-white','text-home-color');
 
    donationBtn.classList.add('bg-white', 'text-home-color');
-   donationBtn.classList.remove('bg-btn-color')
+   donationBtn.classList.remove('bg-btn-color','hover:bg-btn-color')
  })
 
 
@@ -41,9 +66,9 @@ function donateMoney(event) {
     document.getElementById('history-section').classList.add('hidden');
 
     historyBtn.classList.add('bg-white','text-home-color');
-    historyBtn.classList.remove('bg-btn-color');
+    historyBtn.classList.remove('bg-btn-color','hover:bg-btn-color');
  
-    donationBtn.classList.add('bg-btn-color');
+    donationBtn.classList.add('bg-btn-color','hover:bg-btn-color');
     donationBtn.classList.remove('bg-white','text-home-color')
  })
 
